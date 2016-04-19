c9585ff97e05feda7ff09223474f21b7a792382f
enridaga <enricodaga@gmail.com>
2016-04-19 16:42:39 +0100

    changes to FeatureSet. Moved logging to log4j2:

--- core/src/main/java/dinowolf/features/FeatureSet.java
+++ core/src/main/java/dinowolf/features/FeatureSet.java
@@ -2,8 +2,6 @@ package dinowolf.features;
 
 import java.util.Set;
 
-import org.apache.taverna.scufl2.api.container.WorkflowBundle;
-
 public interface FeatureSet {
 
 	public Set<String> getPortPairs();
